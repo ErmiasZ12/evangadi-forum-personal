56e10e64b3972a2dfd70d4e3347653e6c75b2a6c
Selomie@fkre21 <selomielamesgin23@gmail.com>
2025-12-29 23:10:32 -0800

    single question page

--- Server/routes/questionRoute.js
+++ Server/routes/questionRoute.js
@@ -1,10 +1,17 @@
 
+const express = require("express");
+const router = express.Router();
 
-const {getAllQuestions} = require("../controller/questionController");
+const {
+  getAllQuestions,
+  getSingleQuestion,
+} = require("../controller/questionController");
 
 // routes to GET /api/question, check User must be logged in, then calls getAllquestions
-
 router.get("/", authMiddleware, getAllQuestions);
+
+// GET single question
+router.get("/:question_id", getSingleQuestion);
 
 
 module.exports = router;
