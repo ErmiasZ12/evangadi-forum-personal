0e9bd0b78ef3d153de2df4cb1a0e6868e9670898
Selomie@fkre21 <selomielamesgin23@gmail.com>
2026-01-07 13:15:39 -0800

    add email utils and forgot password function

--- Server/routes/userRoute.js
+++ Server/routes/userRoute.js
@@ -6,10 +6,17 @@ const {
   createUser,
   login,
   checkUser,
+  forgotPassword,
+  resetPassword,
 } = require("../controller/userController");
 
 router.post("/register", createUser);
 router.post("/login", login);
+
+// forgot password
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password/:token", resetPassword);
+
 router.get("/check", authMiddleware, checkUser);
 
 module.exports = router;
